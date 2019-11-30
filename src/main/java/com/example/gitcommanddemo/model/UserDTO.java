fc1187c4d96c0533ef48d0acf1c288d19cb6ebac
yaokai <yaokai@01zhuanche.com>
2019-11-30 16:20:14 +0800

    合并提交1-3
    初始化
    
    提交1
    
    提交2
    
    提交3

---
+++ src/main/java/com/example/gitcommanddemo/model/UserDTO.java
@@ -0,0 +1,10 @@
+package com.example.gitcommanddemo.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class UserDTO implements Serializable {
+    private Long id;
+}
---
+++ src/main/java/com/example/gitcommanddemo/service/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.gitcommanddemo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
