8a805c2e0543e1608b69b5af288d324a78b1ae54
yaokai <yaokai@01zhuanche.com>
2019-11-30 16:28:39 +0800

    测试方法

--- src/test/java/com/example/gitcommanddemo/GitCommandDemoApplicationTests.java
+++ src/test/java/com/example/gitcommanddemo/GitCommandDemoApplicationTests.java
@@ -1,10 +1,15 @@
 package com.example.gitcommanddemo;
 
+import com.example.gitcommanddemo.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GitCommandDemoApplicationTests {
+
+    @Autowired
+    private UserService userService;
 
     @Test
     void contextLoads() {
