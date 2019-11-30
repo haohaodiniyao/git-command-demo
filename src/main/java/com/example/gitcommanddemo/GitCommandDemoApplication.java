9a446a93afb089c249e97a9d6e585dd739b07714
yaokai <yaokai@01zhuanche.com>
2019-11-30 16:20:14 +0800

    初始化

---
+++ src/main/java/com/example/gitcommanddemo/GitCommandDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitcommanddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCommandDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitCommandDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitcommanddemo/GitCommandDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitcommanddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCommandDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
