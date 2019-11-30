d9f51188972c1567c47557c99357bc8822eaa635
yaokai <yaokai@01zhuanche.com>
2019-11-30 16:27:48 +0800

    提交3

--- src/main/java/com/example/gitcommanddemo/controller/UserController.java
+++ src/main/java/com/example/gitcommanddemo/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.example.gitcommanddemo;
+package com.example.gitcommanddemo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/com/example/gitcommanddemo/service/UserService.java
@@ -0,0 +1,4 @@
+package com.example.gitcommanddemo.service;
+
+public interface UserService {
+}
